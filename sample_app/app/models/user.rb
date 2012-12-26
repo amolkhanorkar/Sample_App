bf62d28227a95e91de12ee0fa5d2cba36fec0bca
Amol <webonise@amol-desktop.(none)>
2012-12-26 11:05:37 +0530

    Just for Refresh

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -43,16 +43,18 @@ class User < ActiveRecord::Base
     relationships.find_by_followed_id(other_user.id).destroy
   end
 
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.from_users_followed_by(self)
+  end
+
   private
 
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
   end
 
-  def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.from_users_followed_by(self)
-  end
+
 
 
 end
